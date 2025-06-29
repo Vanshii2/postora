3ef198e4b2482b8f1749b1ec6ba88d55d6c5cc7f
Vanshita Tripathi <vanshupro99@gmail.com>
2025-06-29 18:27:43 +0530

    Refactor Posts component to use React Query for data fetching and add feedType prop for dynamic endpoint selection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,9 +12,6 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
     },
   }
  });
-
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
